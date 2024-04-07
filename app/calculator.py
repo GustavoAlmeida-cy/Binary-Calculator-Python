c8e06e843e682431a80650ab31ceaf7715441d0d
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2024-04-07 14:21:32 -0300

    subtraction function

--- app/calculator.py
+++ app/calculator.py
@@ -1,18 +1,18 @@
 # Importing scripts
 from scripts.terminal_colors import tcolors # Enable terminal colors
-from scripts.calc_functions import binary_addition # Bring the calculation functions
+from scripts.calc_functions import binary_addition, binary_subtraction # Bring the calculation functions
 
 # Program title
 print(f"{tcolors.HEADER}-----[ Binary Calculator ]-----\n{tcolors.ENDC}")
 
 # Multiple choice message and logic
 print(f"{tcolors.WARNING}| What type of calculation do you wish? (type the correct number corresponding to the right option){tcolors.ENDC}")
-choice = input((f"{tcolors.WARNING}| 1. Addition\n| 2. Subtract\n\n| : {tcolors.ENDC}"))
+choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtraction\n| 3. Multiplication\n| 4. Division\n\n| : {tcolors.ENDC}"))
 
 if choice != "1" and choice != "2":
     while choice != "1" and choice != "2":
         print(f"{tcolors.FAIL}\n| Invalid choice! Please type other option{tcolors.ENDC}")
-        choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtract\n\n| : {tcolors.ENDC}"))
+        choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtraction\n| 3. Multiplication\n| 4. Division\n\n| : {tcolors.ENDC}"))
 
 # Reading user inputs
 first_num = input(f"{tcolors.HEADER}\n| Type the first number: {tcolors.ENDC}")
@@ -20,6 +20,8 @@ second_num = input(f"{tcolors.HEADER}\n| Type the second number: {tcolors.ENDC}"
 
 if choice == "1":
     binary_addition(first_num, second_num)
+elif choice == "2":
+    binary_subtraction(first_num, second_num)
 
 # Program end
 print(f"{tcolors.HEADER}\n-------[ PROGRAM END ]-------{tcolors.ENDC}")
