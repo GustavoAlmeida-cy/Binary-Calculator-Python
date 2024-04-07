d5d960f6c1343a493f4b1569cc8e5be39fd1955c
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2024-04-07 14:28:24 -0300

    multiplication function

--- app/scripts/calc_functions.py
+++ app/scripts/calc_functions.py
@@ -30,3 +30,18 @@ def binary_subtraction(first_b_num:str, second_b_num:str): # Subtraction functio
     binary_result = decimal_to_binary(decimal_result)
 
     return print(f"{tcolors.OKCYAN}\n| [Decimal result]: The addition of {first_d_num} - {second_d_num} = {decimal_result}{tcolors.ENDC}"), print(f"{tcolors.OKGREEN}\n| [Binary result]: The addition of {first_b_num} - {second_b_num} = {binary_result}\n{tcolors.ENDC}")
+
+def binary_multiplication(first_b_num:str, second_b_num:str): # Subtraction function
+    # print(f"{tcolors.WARNING}\n - Before decimal conversion\n First number: {first_b_num}\n Second number: {second_b_num}\n{tcolors.ENDC}") # Debug comment!
+    
+    # Converting Binary to Decimal
+    first_d_num = binary_to_decimal(first_b_num)
+    second_d_num = binary_to_decimal(second_b_num)
+    
+    # print(f"{tcolors.WARNING}\n - After decimal conversion\n First number: {first_d_num}\n Second number: {second_d_num}\n{tcolors.ENDC}") # Debug comment!
+    
+    decimal_result = first_d_num * second_d_num
+    binary_result = decimal_to_binary(decimal_result)
+
+    return print(f"{tcolors.OKCYAN}\n| [Decimal result]: The addition of {first_d_num} * {second_d_num} = {decimal_result}{tcolors.ENDC}"), print(f"{tcolors.OKGREEN}\n| [Binary result]: The addition of {first_b_num} * {second_b_num} = {binary_result}\n{tcolors.ENDC}")
+
