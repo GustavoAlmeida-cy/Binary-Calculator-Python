48a66094b9505ffafe88166862992bfe21909a33
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2024-04-07 14:33:34 -0300

    division function

--- app/calculator.py
+++ app/calculator.py
@@ -1,6 +1,6 @@
 # Importing scripts
 from scripts.terminal_colors import tcolors # Enable terminal colors
-from scripts.calc_functions import binary_addition, binary_subtraction, binary_multiplication # Bring the calculation functions
+from scripts.calc_functions import binary_addition, binary_subtraction, binary_multiplication, binary_division # Bring the calculation functions
 
 # Program title
 print(f"{tcolors.HEADER}-----[ Binary Calculator ]-----\n{tcolors.ENDC}")
@@ -9,8 +9,8 @@ print(f"{tcolors.HEADER}-----[ Binary Calculator ]-----\n{tcolors.ENDC}")
 print(f"{tcolors.WARNING}| What type of calculation do you wish? (type the correct number corresponding to the right option){tcolors.ENDC}")
 choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtraction\n| 3. Multiplication\n| 4. Division\n\n| : {tcolors.ENDC}"))
 
-if choice != "1" and choice != "2" and choice != "3":
-    while choice != "1" and choice != "2" and choice != "3":
+if choice != "1" and choice != "2" and choice != "3" and choice != "4":
+    while choice != "1" and choice != "2" and choice != "3" and choice != "4":
         print(f"{tcolors.FAIL}\n| Invalid choice! Please type other option{tcolors.ENDC}")
         choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtraction\n| 3. Multiplication\n| 4. Division\n\n| : {tcolors.ENDC}"))
 
@@ -24,8 +24,8 @@ elif choice == "2":
     binary_subtraction(first_num, second_num)
 elif choice == "3":
     binary_multiplication(first_num, second_num)
-# elif choice == "4":
-#     binary_division(first_num, second_num)
+elif choice == "4":
+    binary_division(first_num, second_num)
 
 # Program end
 print(f"{tcolors.HEADER}\n-------[ PROGRAM END ]-------{tcolors.ENDC}")
--- app/scripts/calc_functions.py
+++ app/scripts/calc_functions.py
@@ -31,7 +31,7 @@ def binary_subtraction(first_b_num:str, second_b_num:str): # Subtraction functio
 
     return print(f"{tcolors.OKCYAN}\n| [Decimal result]: The addition of {first_d_num} - {second_d_num} = {decimal_result}{tcolors.ENDC}"), print(f"{tcolors.OKGREEN}\n| [Binary result]: The addition of {first_b_num} - {second_b_num} = {binary_result}\n{tcolors.ENDC}")
 
-def binary_multiplication(first_b_num:str, second_b_num:str): # Subtraction function
+def binary_multiplication(first_b_num:str, second_b_num:str): # Multiplication function
     # print(f"{tcolors.WARNING}\n - Before decimal conversion\n First number: {first_b_num}\n Second number: {second_b_num}\n{tcolors.ENDC}") # Debug comment!
     
     # Converting Binary to Decimal
@@ -44,4 +44,18 @@ def binary_multiplication(first_b_num:str, second_b_num:str): # Subtraction func
     binary_result = decimal_to_binary(decimal_result)
 
     return print(f"{tcolors.OKCYAN}\n| [Decimal result]: The addition of {first_d_num} * {second_d_num} = {decimal_result}{tcolors.ENDC}"), print(f"{tcolors.OKGREEN}\n| [Binary result]: The addition of {first_b_num} * {second_b_num} = {binary_result}\n{tcolors.ENDC}")
+
+def binary_division(first_b_num:str, second_b_num:str): # Division function
+    # print(f"{tcolors.WARNING}\n - Before decimal conversion\n First number: {first_b_num}\n Second number: {second_b_num}\n{tcolors.ENDC}") # Debug comment!
+    
+    # Converting Binary to Decimal
+    first_d_num = binary_to_decimal(first_b_num)
+    second_d_num = binary_to_decimal(second_b_num)
+    
+    # print(f"{tcolors.WARNING}\n - After decimal conversion\n First number: {first_d_num}\n Second number: {second_d_num}\n{tcolors.ENDC}") # Debug comment!
+    
+    decimal_result = first_d_num // second_d_num
+    binary_result = decimal_to_binary(decimal_result)
+
+    return print(f"{tcolors.OKCYAN}\n| [Decimal result]: The addition of {first_d_num} / {second_d_num} = {decimal_result}{tcolors.ENDC}"), print(f"{tcolors.OKGREEN}\n| [Binary result]: The addition of {first_b_num} / {second_b_num} = {binary_result}\n{tcolors.ENDC}")
 
