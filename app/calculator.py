34bf29e6ef9d600f7da342b3a09d1db95ef28b98
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2024-04-07 14:11:27 -0300

    refactor v1 and multiple choice logic

--- app/calculator.py
+++ app/calculator.py
@@ -1,31 +1,25 @@
 # Importing scripts
-from terminal_colors import tcolors # Enable terminal colors
-from conversions import binary_to_decimal, decimal_to_binary # Bring the conversions functions
+from scripts.terminal_colors import tcolors # Enable terminal colors
+from scripts.calc_functions import binary_addition # Bring the calculation functions
 
 # Program title
 print(f"{tcolors.HEADER}-----[ Binary Calculator ]-----\n{tcolors.ENDC}")
 
-# Addition function
-def binary_addition(first_b_num:str, second_b_num:str):
-    # print(f"{tcolors.WARNING}\n - Before decimal conversion\n First number: {first_b_num}\n Second number: {second_b_num}\n{tcolors.ENDC}") # Debug comment!
-    
-    # Converting Binary to Decimal
-    first_d_num = binary_to_decimal(first_b_num)
-    second_d_num = binary_to_decimal(second_b_num)
-    
-    # print(f"{tcolors.WARNING}\n - After decimal conversion\n First number: {first_d_num}\n Second number: {second_d_num}\n{tcolors.ENDC}") # Debug comment!
-    
-    decimal_result = first_d_num + second_d_num
-    binary_result = decimal_to_binary(decimal_result)
+# Multiple choice message and logic
+print(f"{tcolors.WARNING}| What type of calculation do you wish? (type the correct number corresponding to the right option){tcolors.ENDC}")
+choice = input((f"{tcolors.WARNING}| 1. Addition\n| 2. Subtract\n\n| : {tcolors.ENDC}"))
 
-    print(f"{tcolors.OKCYAN}\n [Decimal result]: The addition of {first_d_num} + {second_d_num} = {decimal_result}{tcolors.ENDC}")
-    print(f"{tcolors.OKGREEN}\n [Binary result]: The addition of {first_b_num} + {second_b_num} = {binary_result}\n{tcolors.ENDC}")
+if choice != "1" and choice != "2":
+    while choice != "1" and choice != "2":
+        print(f"{tcolors.FAIL}\n| Invalid choice! Please type other option{tcolors.ENDC}")
+        choice = input((f"{tcolors.WARNING}\n| 1. Addition\n| 2. Subtract\n\n| : {tcolors.ENDC}"))
 
 # Reading user inputs
-first_num = input(f"{tcolors.HEADER}\n Type the first number: {tcolors.ENDC}")
-second_num = input(f"{tcolors.HEADER}\n Type the second number: {tcolors.ENDC}")
+first_num = input(f"{tcolors.HEADER}\n| Type the first number: {tcolors.ENDC}")
+second_num = input(f"{tcolors.HEADER}\n| Type the second number: {tcolors.ENDC}")
 
-binary_addition(first_num, second_num)
+if choice == "1":
+    binary_addition(first_num, second_num)
 
 # Program end
 print(f"{tcolors.HEADER}\n-------[ PROGRAM END ]-------{tcolors.ENDC}")
--- app/scripts/convert_functions.py
+++ app/scripts/convert_functions.py
@@ -1,10 +1,10 @@
-# Converts binary numberns in decimal numbers
-def binary_to_decimal(binary_num:str):
+# Functions
+def binary_to_decimal(binary_num:str): # Converts binary numbers in decimal numbers
     decimal_num = int(binary_num, 2)
     return decimal_num
 
 # Converts decimal numberns in binary numbers
-def decimal_to_binary(decimal_num:int):
+def decimal_to_binary(decimal_num:int): # Convers decimal numbers in binary numbers
     if decimal_num == 0:
         return '0'
     
